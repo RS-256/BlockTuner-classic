c3201673574a8aad1148b735bfd15f8279279d05
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-05-16 20:17:06 -0400

    Set compelete conditional

--- src/main/java/io/github/lumine1909/blocktuner/mixin/CommandSuggestionsMixin.java
+++ src/main/java/io/github/lumine1909/blocktuner/mixin/CommandSuggestionsMixin.java
@@ -1,8 +1,13 @@
 package io.github.lumine1909.blocktuner.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.mojang.brigadier.suggestion.Suggestion;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.components.CommandSuggestions;
+import net.minecraft.client.gui.screens.Screen;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -10,22 +15,46 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Environment(EnvType.CLIENT)
 @Mixin(CommandSuggestions.SuggestionsList.class)
-public class CommandSuggestionsMixin {
+public abstract class CommandSuggestionsMixin {
 
     @Final
     @Shadow
     CommandSuggestions field_21615;
 
+    @Shadow
+    public abstract void cycle(int i);
+
+    @Shadow
+    boolean tabCycles;
+
+    @Shadow
+    @Final
+    private List<Suggestion> suggestionList;
+
+    @Shadow
+    private int current;
+
     @Inject(
         method = "useSuggestion",
         at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/components/EditBox;setValue(Ljava/lang/String;)V"
+            value = "FIELD",
+            target = "Lnet/minecraft/client/gui/components/CommandSuggestions;keepSuggestions:Z",
+            opcode = Opcodes.PUTFIELD,
+            ordinal = 0,
+            shift = At.Shift.AFTER
         )
     )
-    private void injected(CallbackInfo ci) {
-        field_21615.keepSuggestions = false;
+    private void injected(CallbackInfo ci, @Local LocalRef<Suggestion> suggestion) {
+        if (Screen.hasControlDown()) {
+            if (this.tabCycles) {
+                this.cycle(Screen.hasShiftDown() ? 1 : -1);
+                suggestion.set(suggestionList.get(this.current));
+            }
+            field_21615.keepSuggestions = false;
+        }
     }
 }
