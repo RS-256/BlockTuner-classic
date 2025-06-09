f4796de0f53ea502d2b3c982509c27cfb3610e6a
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2025-06-09 17:23:57 +0800

    Update to v1.3.0, remove sound pitch boundaries

---
+++ src/main/java/io/github/lumine1909/blocktuner/mixin/PlaySoundCommandMixin.java
@@ -0,0 +1,23 @@
+package io.github.lumine1909.blocktuner.mixin;
+
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import net.minecraft.server.commands.PlaySoundCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PlaySoundCommand.class)
+public class PlaySoundCommandMixin {
+
+    @Redirect(
+        method = "source",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/mojang/brigadier/arguments/FloatArgumentType;floatArg(FF)Lcom/mojang/brigadier/arguments/FloatArgumentType;",
+            ordinal = 0
+        )
+    )
+    private static FloatArgumentType source(float min, float max) {
+        return FloatArgumentType.floatArg(0);
+    }
+}
---
+++ src/main/java/io/github/lumine1909/blocktuner/mixin/SoundEngineMixin.java
@@ -0,0 +1,22 @@
+package io.github.lumine1909.blocktuner.mixin;
+
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundEngine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SoundEngine.class)
+public class SoundEngineMixin {
+
+    @Inject(
+        method = "calculatePitch",
+        at = @At("HEAD"),
+        cancellable = true
+    )
+    private void calculatePitch(SoundInstance instance, CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(instance.getPitch());
+        cir.cancel();
+    }
+}
